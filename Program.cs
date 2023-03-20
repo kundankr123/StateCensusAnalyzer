a49137ea8427da9042d7c69ac830723d6cfae7dc
kundan <goswamikundan123@gmail.com>
2023-03-20 20:15:57 +0530

    Adding file which displays greeting message on master branch.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StateCensus
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to State Census Analyser Problem");
+        }
+    }
+}
