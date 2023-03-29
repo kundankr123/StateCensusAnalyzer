9e985e1060dfcc415f369572fccac1bd8bbb6f5a
kundan <goswamikundan123@gmail.com>
2023-03-29 17:14:06 +0530

    Adding test files which throw execption upon wrong file formate.

--- StateCensusTest/UnitTest1.cs
+++ StateCensusTest/UnitTest1.cs
@@ -19,5 +19,17 @@ namespace StateCensusTest
             // Assert
             Assert.AreEqual(expectedNumberOfRecords, actualNumberOfRecords);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void LoadDataFromCSV_ShouldThrowException_WhenIncorrectFileIsProvided()
+        {
+            // ArrangectFile.csv
+            string filePath = "C:\\Users\\kundan\\Desktop\\Bridgelab\\rfp257\\StateCensus\\StateCensus\\Incorrectdata.csv";
+            StateCensusAnalyzer analyser = new StateCensusAnalyzer(filePath);
+
+            // Act and Assert
+            analyser.LoadDataFromCSV();
+        }
     }
 }
